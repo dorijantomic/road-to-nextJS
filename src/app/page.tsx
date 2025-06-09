5718bf023c1ff7310ae5a1b7b9150279b2ebf1a9
dorijantomic <dorijan11@gmail.com>
2025-06-09 20:17:54 +0200

    Copy code directly from source as i cant be bothered by what i need not learn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
-import { ticketsPath } from "@/path";
 import Link from "next/link";
+import { Heading } from "@/components/heading";
+import { ticketsPath } from "@/path";
 
 const HomePage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
-      <div>
-        <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
-        <p className="text-sm text-muted-foreground">
-          Your home place to start
-        </p>
-      </div>
+      <Heading title="Home" description="Your home place to start" />
+
       <div className="flex-1 flex flex-col items-center">
-        <Link className="underline" href={ticketsPath()}>
-          Tickets
+        <Link href={ticketsPath()} className="text-sm underline">
+          Go to Tickets
         </Link>
       </div>
     </div>
--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,4 +1,9 @@
+import Link from "next/link";
+import { Placeholder } from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
 import { initialTickets } from "@/data";
+import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { ticketsPath } from "@/path";
 
 type TicketPageProps = {
   params: Promise<{
@@ -8,14 +13,25 @@ type TicketPageProps = {
 
 const TicketPage = async ({ params }: TicketPageProps) => {
   const { ticketId } = await params;
+
   const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
-  if (!ticket) return <div>Ticket not found</div>;
-  const { title, content, status } = ticket;
+
+  if (!ticket) {
+    return (
+      <Placeholder
+        label="Ticket not found"
+        button={
+          <Button asChild variant="outline">
+            <Link href={ticketsPath()}>Go to Tickets</Link>
+          </Button>
+        }
+      />
+    );
+  }
+
   return (
-    <div>
-      <h2 className="text-lg">{title}</h2>
-      <p className="text-sm">{content}</p>
-      <span>{status}</span>
+    <div className="flex justify-center animate-fade-from-top">
+      <TicketItem ticket={ticket} isDetail />
     </div>
   );
 };
